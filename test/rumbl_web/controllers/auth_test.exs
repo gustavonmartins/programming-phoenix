ebe52173df9c5553ea164ddf7ef07c9162a2d97f
Gustavo Martins <martins.yo@gmail.com>
2019-09-26 18:46:13 +0200

    Page 169

---
+++ test/rumbl_web/controllers/auth_test.exs
@@ -0,0 +1,30 @@
+defmodule RumblWeb.AuthTest do
+  use RumblWeb.ConnCase, async: true
+  alias RumblWeb.Auth
+
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> bypass_through(RumblWeb.Router, :browser)
+      |> get("/")
+
+    {:ok, %{conn: conn}}
+  end
+
+  test "authenticate_user halts when no current_user exists", %{conn: conn} do
+    conn =
+      conn
+      |> Auth.authenticate_user([])
+
+    assert conn.halted
+  end
+
+  test "authenticate_user continues when the curernt_user exists", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:current_user, %Rumbl.Accounts.User{})
+      |> Auth.authenticate_user([])
+
+    refute conn.halted
+  end
+end
