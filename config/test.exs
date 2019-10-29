bcb557fb0f650d434734777ac6adc6dc97ae7121
Gustavo Martins <martins.yo@gmail.com>
2019-10-29 07:28:49 +0100

    Page 242

--- config/test.exs
+++ config/test.exs
@@ -10,12 +10,11 @@ config :rumbl, Rumbl.Repo,
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :rumbl, RumblWeb.Endpoint,
+config :rumbl_web, RumblWeb.Endpoint,
   http: [port: 4002],
   server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
 
-#Prevents password hashing from stealing too much time during tests
 config :pbkdf2_elixir, :rounds, 1
